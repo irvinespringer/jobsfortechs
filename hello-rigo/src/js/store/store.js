51d2180ed08776c574514c514c42f71dcef9f544
James Walters <james.walters221@gmail.com>
2019-02-16 16:43:21 +0000

    optimizing store search

--- hello-rigo/src/js/store/store.js
+++ hello-rigo/src/js/store/store.js
@@ -67,22 +67,14 @@ const getState = ({ getStore, setStore }) => {
 					//return (
 					//	item.job_title.search(new RegExp(job_title, "i")) > 0
 					//);
-
-					if (company_location !== "") {
-						return (
-							item.job_title
-								.toLowerCase()
-								.indexOf(job_title.toLowerCase()) > -1 &&
-							item.company_location.toLowerCase() ===
-								company_location.toLowerCase()
-						);
-					} else {
-						return (
-							item.job_title
-								.toLowerCase()
-								.indexOf(job_title.toLowerCase()) > -1
-						);
-					}
+					return (
+						item.job_title
+							.toLowerCase()
+							.indexOf(job_title.toLowerCase()) > -1 &&
+						item.company_location
+							.toLowerCase()
+							.indexOf(company_location.toLowerCase()) > -1
+					);
 
 					//return item.company_location === company_location;
 
